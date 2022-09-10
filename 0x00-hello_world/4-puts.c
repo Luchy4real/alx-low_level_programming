7310f019f190e1c28e5707d2635850eb4e91d096
root <izunobioluchi@gmail.com>
2022-09-09 23:04:15 -0700

    my putting

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - print the string in the main function
- *
- * Description: using the main function
- * this program prints "programming is like building a multilingual puzzle"
- * Return: 0
+ *Description: main - prints out a string to stdout
+ *Return: 0 if success
  */
+
 int main(void)
 {
         puts("\"Programming is like building a multilingual puzzle");
-        return (0);
+
+	return (0);
 }
