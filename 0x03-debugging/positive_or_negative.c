f4f6b4412b965baeef74ed0eeffd29a1e3b9f03b
root <izunobioluchi@gmail.com>
2022-09-15 21:50:11 -0700

    My initial commit

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -9,24 +9,21 @@
 */
 
 /* betty style doc for function main goes there */
-int main(void)
+void positive_or_negative(int i)
 {
-	int n;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
-	if (n > 0)
-	{
-		printf("%i is positive\n", n);
-	}
-	else if (n == 0)
-	{
-		printf("%i is zero\n", n);
-	}
-	else if (n < 0)
-	{
-		printf("%i is negative\n", n);
-	}
-	return (0);
+        /* your code goes there */
+        if (i > 0)
+        {
+                printf("%i is positive\n", i);
+        }
+        else if (i == 0)
+        {
+                printf("%i is zero\n", i);
+        }
+        else if (i < 0)
+        {
+                printf("%i is negative\n", i);
+        }
+
 }
